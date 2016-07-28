3d7e634b5ff14223b435f22d5104f863e3aa7a5b
Morrison Cole <morrison@improbable.io>
2016-07-28 14:14:32 +0100

    Blank project v.1

---
+++ README.md
@@ -0,0 +1,30 @@
+# BlankProject
+---
+
+*Copyright Improbable 2016*
+
+- *Accompanying Tutorial*: [improbable.io/docs/latest/tutorials/your-first-project/lesson1](https://improbable.io/docs/latest/tutorials/your-first-project/lesson1)
+
+- *GitHub Repository*: [github.com/improbable-public/BlankProject](https://github.com/improbable-public/BlankProject)
+
+---
+
+## Introduction
+
+This is a blank SpatialOS project that you can use to build your own SpatialOS application.
+
+It contains:
+
+* A SpatialOS project descriptor.
+* GSim and Unity workers.
+* Bare-bones Launch Configurations for automatically and manually launching workers.
+* Bare-bones Client and Physics Scenes with a `Bootstrap.cs` file.
+* An example deployment configuration, `deploy.pb.json`.
+* `.gitignore` files for a SpatialOS project.
+
+## Using the BlankProject
+
+For instructions on how to use and set-up this BlankProject, follow the Your First Project tutorial, available in
+the [Improbable documentation](https://improbable.io/docs).
+
+To report bugs or to give feedback on this project, please contact SpatialOS Support.
---
+++ workers/unity/Assets/Bootstrap.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using Improbable;
+using Improbable.Unity;
+using Improbable.Core;
+using Improbable.Unity.Core;
+using Improbable.Core.Network;
+using System.Collections.Generic;
+using System;
+using Improbable.Fapi.Receptionist;
+
+public class Bootstrap : MonoBehaviour, IBootstrapHandler {
+
+    public string ReceptionistIp = "localhost";
+    public int ReceptionistPort = 7777;
+    public EnginePlatform EngineType = EnginePlatform.Client;
+    public int FixedUpdateRate = 20;
+    public int TargetFps = 120;
+    public bool UsePrefabPooling = true;
+    public LinkProtocol LinkProtocol = LinkProtocol.Tcp;
+
+    public void Start()
+    {
+        var engineConfiguration = EngineConfiguration.Instance;
+
+        engineConfiguration.AssemblyName = string.Empty;
+        engineConfiguration.Ip = ReceptionistIp;
+        engineConfiguration.Port = ReceptionistPort;
+        engineConfiguration.TargetFps = TargetFps;
+        engineConfiguration.FixedUpdateRate = FixedUpdateRate;
+        engineConfiguration.UsePrefabPooling = UsePrefabPooling;
+        engineConfiguration.PrefabToPool = Prepool();
+        engineConfiguration.EngineType = EngineTypeUtils.ToEngineName(EngineType);
+        engineConfiguration.UseInstrumentation = true;
+        engineConfiguration.IsDebugMode = true;
+        engineConfiguration.LinkProtocol = LinkProtocol;
+        engineConfiguration.MsgProcessLimitPerFrame = 0;
+        
+        engineConfiguration.MetaData = AdditionalMetaData();
+
+        EngineLifecycleManager.StartGame(this, gameObject);
+    }
+
+    private static Dictionary<string, int> Prepool()
+    {
+        return new Dictionary<string, int>();
+    }
+
+    private static Dictionary<string, string> AdditionalMetaData()
+    {
+        return new Dictionary<string, string>();
+    }
+
+    /** IBoostrapHandler methods **/
+
+    public void OnDeploymentListRetrieved(IList<IDeployment> deployments, Action<IDeployment> handleChosenDeployment)
+    {
+        handleChosenDeployment(deployments[0]);
+    }
+
+    public void OnQueuingStarted()
+    {
+        Debug.Log("Queueing started");
+    }
+
+    public void OnQueuingUpdate(IQueueStatus status)
+    {
+        Debug.Log(status);
+    }
+
+    public void OnQueuingCompleted(IQueueStatus status)
+    {
+        Debug.Log("Queueing complete");
+    }
+
+    public void OnBootstrapError(Exception exception)
+    {
+        Debug.LogError("Exception: " + exception.Message);
+    }
+
+    public void BeginPreconnectionTasks(IDeployment deployment, IContainer container, Action onCompletedPreconnectionTasks)
+    {
+        onCompletedPreconnectionTasks();
+    }
+}
