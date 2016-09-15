7d6310c8cd0123e8f6f61fd6830d783b17745e71
Miraan Tabrez <miraan@improbable.io>
2016-09-15 15:11:57 +0100

    completed vive tutorial on 8.0.0

--- workers/gsim/src/main/scala/improbable/natures/Player.scala
+++ workers/gsim/src/main/scala/improbable/natures/Player.scala
@@ -1,13 +1,14 @@
 package improbable.natures
 
-import improbable.behaviours.{DelegateLocalPlayerCheckToClientBehaviour, PrintNameBehaviour}
+import improbable.behaviours.{DelegateLocalPlayerCheckToClientBehaviour, DelegatePlayerControlsToClientBehaviour, PrintNameBehaviour}
 import improbable.corelib.natures.{BaseNature, NatureApplication, NatureDescription}
 import improbable.corelib.util.EntityOwner
 import improbable.corelibrary.transforms.TransformNature
+import improbable.math.Vector3d
 import improbable.papi.engine.EngineId
 import improbable.papi.entity.EntityPrefab
 import improbable.papi.entity.behaviour.EntityBehaviourDescriptor
-import improbable.player.Name
+import improbable.player.{LocalPlayerCheck, Name, PlayerControls}
 
 object Player extends NatureDescription {
 
@@ -16,7 +17,8 @@ object Player extends NatureDescription {
   override def activeBehaviours: Set[EntityBehaviourDescriptor] = {
     Set(
       descriptorOf[PrintNameBehaviour],
-      descriptorOf[DelegateLocalPlayerCheckToClientBehaviour]
+      descriptorOf[DelegateLocalPlayerCheckToClientBehaviour],
+      descriptorOf[DelegatePlayerControlsToClientBehaviour]
     )
   }
 
@@ -24,7 +26,16 @@ object Player extends NatureDescription {
     application(
       states = Seq(
         Name(playerName),
-        EntityOwner(Some(clientId))
+        LocalPlayerCheck(),
+        EntityOwner(Some(clientId)),
+        PlayerControls(
+          Vector3d(0,0,0),
+          Vector3d(0,0,0),
+          Vector3d(0,0,0),
+          Vector3d(0,0,0),
+          Vector3d(0,0,0),
+          Vector3d(0,0,0)
+        )
       ),
       natures = Seq(
         BaseNature(entityPrefab = EntityPrefab("Player"), isPhysical = true),
---
+++ workers/unity/Assets/ModelSender.cs
@@ -0,0 +1,38 @@
+﻿using Improbable.Player;
+using Improbable.Unity.Common.Core.Math;
+using Improbable.Unity.Visualizer;
+using UnityEngine;
+
+public class ModelSender : MonoBehaviour {
+    [Require]
+    protected PlayerControlsWriter PlayerControls;
+
+    public Transform DeviceLeftHandTransform;
+    public Transform DeviceRightHandTransform;
+    public Transform DeviceHeadTransform;
+
+    void Update()
+    {
+        if (DeviceLeftHandTransform.gameObject.activeSelf)
+        {
+            PlayerControls.Update
+                .LeftHandPosition(DeviceLeftHandTransform.position.ToNativeVector())
+                .LeftHandRotation(DeviceLeftHandTransform.rotation.eulerAngles.ToNativeVector())
+                .FinishAndSend();
+        }
+        if (DeviceRightHandTransform.gameObject.activeSelf)
+        {
+            PlayerControls.Update
+                .RightHandPosition(DeviceRightHandTransform.position.ToNativeVector())
+                .RightHandRotation(DeviceRightHandTransform.rotation.eulerAngles.ToNativeVector())
+                .FinishAndSend();
+        }
+        if (DeviceHeadTransform.gameObject.activeSelf)
+        {
+            PlayerControls.Update
+                .HeadPosition(DeviceHeadTransform.position.ToNativeVector())
+                .HeadRotation(DeviceHeadTransform.rotation.eulerAngles.ToNativeVector())
+                .FinishAndSend();
+        }
+    }
+}
